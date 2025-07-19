0f5d708be960120acde52d95af2f902309d59083
acth2 <iliane57950@gmail.com>
2025-07-19 17:05:16 +0200

    Ajout du repository

---
+++ src/com/massivecraft/factions/integration/herochat/ChannelFactionsAllies.java
@@ -0,0 +1,157 @@
+package com.massivecraft.factions.integration.herochat;
+
+import com.dthielke.herochat.Channel;
+import com.dthielke.herochat.SpamTracker;
+import com.dthielke.herochat.TagFormatter;
+import com.massivecraft.factions.Rel;
+import com.massivecraft.factions.entity.MConf;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+import java.util.EnumSet;
+import java.util.Set;
+import java.util.UUID;
+
+public class ChannelFactionsAllies extends ChannelFactionsAbstract
+{
+	public static final Set<Rel> targetRelations = EnumSet.of(Rel.MEMBER, Rel.RECRUIT, Rel.ALLY);
+	@Override public Set<Rel> getTargetRelations() { return targetRelations; }
+	
+	@Override public String getName() { return MConf.get().herochatAlliesName; }
+	
+	@Override public String getNick() { return MConf.get().herochatAlliesNick; }
+	@Override public void setNick(String nick) { MConf.get().herochatAlliesNick = nick; }
+	
+	@Override public String getFormat() { return MConf.get().herochatAlliesFormat; }
+
+	@Override
+	public Set<UUID> getModeratorsUUID() {
+		return Set.of();
+	}
+
+	@Override
+	public Set<UUID> getMutesUUID() {
+		return Set.of();
+	}
+
+	@Override public void setFormat(String format) { MConf.get().herochatAlliesFormat = format; }
+
+	@Override
+	public void setModerator(UUID uuid, boolean b) {
+
+	}
+
+	@Override
+	public void setModeratorsUUID(Set<UUID> set) {
+
+	}
+
+	@Override
+	public void setMuted(UUID uuid, boolean b) {
+
+	}
+
+	@Override
+	public void setMutesUUID(Set<UUID> set) {
+
+	}
+
+	@Override public ChatColor getColor() { return MConf.get().herochatAlliesColor; }
+	@Override public void setColor(ChatColor color) { MConf.get().herochatAlliesColor = color; }
+	
+	@Override public int getDistance() { return MConf.get().herochatAlliesDistance; }
+	@Override public void setDistance(int distance) { MConf.get().herochatAlliesDistance = distance; }
+	
+	@Override public void addWorld(String world) { MConf.get().herochatAlliesWorlds.add(world); }
+
+	@Override
+	public Set<UUID> getBansUUID() {
+		return Set.of();
+	}
+
+	@Override public Set<String> getWorlds() { return MConf.get().herochatAlliesWorlds; }
+
+	@Override
+	public boolean isBanned(UUID uuid) {
+		return false;
+	}
+
+	@Override public void setWorlds(Set<String> worlds) { MConf.get().herochatAlliesWorlds = worlds; }
+	
+	@Override public boolean isShortcutAllowed() { return MConf.get().herochatAlliesIsShortcutAllowed; }
+
+	@Override
+	public boolean isCrossServer() {
+		return false;
+	}
+
+	@Override
+	public SpamTracker getSpamTracker() {
+		return null;
+	}
+
+	@Override
+	public int getSpamMessageLimit() {
+		return 0;
+	}
+
+	@Override
+	public int getSpamMessageLimitBuildOff() {
+		return 0;
+	}
+
+	@Override
+	public void setSpamMessageLimit(int i) {
+
+	}
+
+	@Override
+	public void setSpamMessageLimitBuildOff(int i) {
+
+	}
+
+	@Override
+	public void setBanned(UUID uuid, boolean b) {
+
+	}
+
+	@Override
+	public void setBansUUID(Set<UUID> set) {
+
+	}
+
+	@Override public void setShortcutAllowed(boolean shortcutAllowed) { MConf.get().herochatAlliesIsShortcutAllowed = shortcutAllowed; }
+	
+	@Override public boolean isCrossWorld() { return MConf.get().herochatAlliesCrossWorld; }
+
+	@Override
+	public boolean isModerator(UUID uuid) {
+		return false;
+	}
+
+	@Override
+	public boolean isMuted(UUID uuid) {
+		return false;
+	}
+
+	@Override public void setCrossWorld(boolean crossWorld) { MConf.get().herochatAlliesCrossWorld = crossWorld; }
+
+	@Override
+	public void setCrossServer(boolean b, boolean b1) {
+
+	}
+
+	@Override public boolean isMuted() { return MConf.get().herochatAlliesMuted; }
+
+	@Override
+	public boolean addTag(String s, TagFormatter tagFormatter) {
+		return false;
+	}
+
+	@Override public void setMuted(boolean value) { MConf.get().herochatAlliesMuted = value; }
+
+	@Override
+	public String formatTag(String s, Player player, Channel channel) {
+		return "";
+	}
+}
---
+++ src/com/massivecraft/factions/integration/herochat/ChannelFactionsFaction.java
@@ -0,0 +1,158 @@
+package com.massivecraft.factions.integration.herochat;
+
+import com.dthielke.herochat.Channel;
+import com.dthielke.herochat.SpamTracker;
+import com.dthielke.herochat.TagFormatter;
+import com.massivecraft.factions.Rel;
+import com.massivecraft.factions.entity.MConf;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+import java.util.EnumSet;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+public class ChannelFactionsFaction extends ChannelFactionsAbstract
+{
+	public static final Set<Rel> targetRelations = EnumSet.of(Rel.MEMBER, Rel.RECRUIT);
+	@Override public Set<Rel> getTargetRelations() { return targetRelations; }
+	
+	@Override public String getName() { return MConf.get().herochatFactionName; }
+	
+	@Override public String getNick() { return MConf.get().herochatFactionNick; }
+	@Override public void setNick(String nick) { MConf.get().herochatFactionNick = nick; }
+	
+	@Override public String getFormat() { return MConf.get().herochatFactionFormat; }
+
+	@Override
+	public Set<UUID> getModeratorsUUID() {
+		return Set.of();
+	}
+
+	@Override
+	public Set<UUID> getMutesUUID() {
+		return Set.of();
+	}
+
+	@Override public void setFormat(String format) { MConf.get().herochatFactionFormat = format; }
+
+	@Override
+	public void setModerator(UUID uuid, boolean b) {
+
+	}
+
+	@Override
+	public void setModeratorsUUID(Set<UUID> set) {
+
+	}
+
+	@Override
+	public void setMuted(UUID uuid, boolean b) {
+
+	}
+
+	@Override
+	public void setMutesUUID(Set<UUID> set) {
+
+	}
+
+	@Override public ChatColor getColor() { return MConf.get().herochatFactionColor; }
+	@Override public void setColor(ChatColor color) { MConf.get().herochatFactionColor = color; }
+	
+	@Override public int getDistance() { return MConf.get().herochatFactionDistance; }
+	@Override public void setDistance(int distance) { MConf.get().herochatFactionDistance = distance; }
+	
+	@Override public void addWorld(String world) { MConf.get().herochatFactionWorlds.add(world); }
+
+	@Override
+	public Set<UUID> getBansUUID() {
+		return Set.of();
+	}
+
+	@Override public Set<String> getWorlds() { return new HashSet<>(MConf.get().herochatFactionWorlds); }
+
+	@Override
+	public boolean isBanned(UUID uuid) {
+		return false;
+	}
+
+	@Override public void setWorlds(Set<String> worlds) { MConf.get().herochatFactionWorlds = worlds; }
+	
+	@Override public boolean isShortcutAllowed() { return MConf.get().herochatFactionIsShortcutAllowed; }
+
+	@Override
+	public boolean isCrossServer() {
+		return false;
+	}
+
+	@Override
+	public SpamTracker getSpamTracker() {
+		return null;
+	}
+
+	@Override
+	public int getSpamMessageLimit() {
+		return 0;
+	}
+
+	@Override
+	public int getSpamMessageLimitBuildOff() {
+		return 0;
+	}
+
+	@Override
+	public void setSpamMessageLimit(int i) {
+
+	}
+
+	@Override
+	public void setSpamMessageLimitBuildOff(int i) {
+
+	}
+
+	@Override
+	public void setBanned(UUID uuid, boolean b) {
+
+	}
+
+	@Override
+	public void setBansUUID(Set<UUID> set) {
+
+	}
+
+	@Override public void setShortcutAllowed(boolean shortcutAllowed) { MConf.get().herochatFactionIsShortcutAllowed = shortcutAllowed; }
+	
+	@Override public boolean isCrossWorld() { return MConf.get().herochatFactionCrossWorld; }
+
+	@Override
+	public boolean isModerator(UUID uuid) {
+		return false;
+	}
+
+	@Override
+	public boolean isMuted(UUID uuid) {
+		return false;
+	}
+
+	@Override public void setCrossWorld(boolean crossWorld) { MConf.get().herochatFactionCrossWorld = crossWorld; }
+
+	@Override
+	public void setCrossServer(boolean b, boolean b1) {
+
+	}
+
+	@Override public boolean isMuted() { return MConf.get().herochatFactionMuted; }
+
+	@Override
+	public boolean addTag(String s, TagFormatter tagFormatter) {
+		return false;
+	}
+
+	@Override public void setMuted(boolean value) { MConf.get().herochatFactionMuted = value; }
+
+	@Override
+	public String formatTag(String s, Player player, Channel channel) {
+		return "";
+	}
+}
